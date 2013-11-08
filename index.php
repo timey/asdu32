f56f724ce1f8bd53d8ad4ef449edea06670d2195
heisaseisa <christianseiser90@gmail.com>
2013-11-08 11:11:14 +0100

    Test Update
    Leerzeichen eingefuegt, cool

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
    		</div>
 	<?php endwhile; ?>
 	
-	<p align="center"><?php next_posts_link('&laquo; &Auml;ltere Eintr&auml;ge') ?> | <?php previous_posts_link('Neuere Eintr&auml;ge &raquo;') ?></p>
+	<p align="center"><?php next_posts_link('&laquo; &Auml;ltere Eintr&auml;ge ') ?> | <?php previous_posts_link('Neuere Eintr&auml;ge &raquo;') ?></p>
 
 	<?php endif; ?>
 
