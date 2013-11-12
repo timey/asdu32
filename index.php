bb8b7fe37bf6383360caababde6fe3b5f2ab58cd
sonjaka <sonja-karl@web.de>
2013-11-12 23:24:45 +0100

    Template Updated
    -Landingpage.css in style.css umgeschichtet
    -Gallery.php angelegt
    -Gallery.php angefangen zu stylen über Code in style.css
    -Header.php angelegt, Header dorthin ausgelagert

--- index.php
+++ index.php
@@ -1,23 +1,18 @@
 <?php get_header(); ?>
 
-   <div id="main">
-   	
-   	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-   		<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-   		<div class="entry">
-      		<?php the_content(); ?>
-   		</div>
-	<?php endwhile; ?>
-	
-	<p align="center"><?php next_posts_link('&laquo; &Auml;ltere Eintr&auml;ge') ?> | <?php previous_posts_link('Neuere Eintr&auml;ge &raquo;') ?></p>
+		<div id="main">
+			<?php if(have_posts()):while(have_posts()):the_post();?>
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				<div class="entry">
+					<?php the_content(); ?>
+				</div>
+			<?php endwhile; ?>
+				<p align="center"><?php next_posts_link('&laquo; &Auml;ltere Eintr&auml;ge') ?> | <?php previous_posts_link('Neuere Eintr&auml;ge &raquo;') ?></p>
+			<?php endif; ?>
 
-	<?php endif; ?>
+		</div><!--main -->
+		<div id="sidebar">
+			<?php get_sidebar(); ?>
+		</div> <!--sidebar-->
 
-
-   </div><!-- main -->
- 
-   <div id="sidebar">
-   		<?php get_sidebar(); ?>
-   </div><!-- sidebar --> 
- 
 <?php get_footer(); ?>
